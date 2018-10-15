fc9e808df0aa8f77afb2fa2de0c29a2c5648c0e2
Prabal Tiwari <prabaltiwari09@gmail.com>
2018-10-15 14:28:26 +0530

    Add files via upload

---
+++ SpeechToText.py
@@ -0,0 +1,12 @@
+import speech_recognition as sr
+
+r=sr.Recognizer()
+
+with sr.Microphone() as source:
+    print("Hey There!! Say Something!!")
+    audio = r.listen(source)
+try:
+    print("Google thinks you said:\n"+r.recognize_google(audio))
+
+except:
+    pass
